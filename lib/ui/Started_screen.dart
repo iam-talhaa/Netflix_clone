0276a8ec5ab648f457bd4a7474fe40e2a22cb139
iam-talhaa <tahakhan4141@gmail.com>
2024-07-15 23:44:21 +0500

    splash Screen and Started Screen

--- lib/ui/Started_screen.dart
+++ lib/ui/Started_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartedScreen extends StatefulWidget {
   const StartedScreen({super.key});
@@ -16,8 +17,11 @@ class _StartedScreenState extends State<StartedScreen> {
         width: MediaQuery.of(context).size.width * 1,
         decoration: BoxDecoration(
             image: DecorationImage(
+                fit: BoxFit.fill,
                 image: AssetImage('Assets/images/S_image.png'))),
         child: Stack(
+          fit: StackFit.loose,
+          clipBehavior: Clip.none,
           children: [
             Container(
               height: MediaQuery.of(context).size.height * 1,
@@ -31,9 +35,63 @@ class _StartedScreenState extends State<StartedScreen> {
                     Color(0xff000000),
                   ])),
             ),
-            Text('    Unlimited'),
-            Text("entertainment"),
-            Text('one low price')
+            Positioned(
+                top: 425,
+                left: 70,
+                child: Text(
+                  '    Unlimited',
+                  style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                )),
+            Positioned(
+              top: 450,
+              left: 70,
+              child: Text(
+                "Entertainment",
+                style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Positioned(
+              top: 474,
+              left: 79,
+              child: Text(
+                'one low price',
+                style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Positioned(
+              top: 510,
+              left: 79,
+              child: Text(
+                'All of Netflix, starting at just\n                   ₹149',
+                style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 16),
+              ),
+            ),
+            Positioned(
+              top: 555,
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width * 1,
+                decoration: BoxDecoration(color: Color(0xffC60A0A)),
+                child: const Center(
+                    child: Text(
+                  'GET STARTED',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                )),
+              ),
+            ),
           ],
         ),
       ),
